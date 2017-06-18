e2f2852453ecd3c87bdf2c9396f144479ada0fbc
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-06-18 13:46:42 +0200

    Show a trade

--- client/src/TradeCard/Client.elm
+++ client/src/TradeCard/Client.elm
@@ -1,13 +1,31 @@
 module TradeCard.Client exposing (main)
 
 import Html
+import TradeCard.Card as Card
 import TradeCard.Collection as Collection
+import TradeCard.Market as Market
 
 main : Html.Html msg
-main = Collection.view collection
+main =
+    let
+        trade = Market.trade collectionA collectionB
 
-collection : Collection.Collection
-collection =
+        aCards = Tuple.first trade
+
+        bCards = Tuple.second trade
+    in
+        Html.div
+            []
+            (List.concat
+                 [
+                  [ Collection.view collectionA ]
+                 , (List.map Card.view aCards)
+                 , (List.map Card.view bCards)
+                 ])
+
+
+collectionA : Collection.Collection
+collectionA =
     let
         result : Result Collection.CollectError Collection.Collection
         result =
@@ -20,6 +38,28 @@ collection =
             |> Result.andThen (Collection.collect { id = 11 })
             |> Result.andThen (Collection.collect { id = 11 })
             |> Result.andThen (Collection.collect { id = 13 })
+            |> Result.andThen (Collection.collect { id = 13 })
+    in
+        case result of
+            Ok c->
+                c
+
+            Err _ ->
+                Collection.empty 1 15
+
+
+collectionB : Collection.Collection
+collectionB =
+    let
+        result : Result Collection.CollectError Collection.Collection
+        result =
+               (Collection.empty 1 15)
+            |> Ok
+            |> Result.andThen (Collection.collect { id = 2 })
+            |> Result.andThen (Collection.collect { id = 2 })
+            |> Result.andThen (Collection.collect { id = 7 })
+            |> Result.andThen (Collection.collect { id = 12 })
+            |> Result.andThen (Collection.collect { id = 12 })
     in
         case result of
             Ok c->
