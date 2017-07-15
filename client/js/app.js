5c76745146ac076a60ef9b2aca093c08bcc2e48e
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-07-15 06:54:10 +0200

    Create a destroyDb function
    This functions allows to reset the database after a restart

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,11 @@
 (function(Elm){
     var container = document.getElementById('container');
     var app = Elm.TradeCard.Client.embed(container);
+
+    window.destroyDb = function(){
+        var db = new PouchDB('card-events');
+        db.destroy().then(function(){
+            console.log('Database \'card-events\' is destroyed');
+        });
+    };
 })(Elm);
